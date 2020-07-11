9dcc4dd40056eda7bdaa2063781158ef1e23f43e
AnamikaMishra-02 <60975676+AnamikaMishra-02@users.noreply.github.com>
2020-07-11 20:19:43 +0530

    Update 1D-Arrays-in-C.c

--- 1D-Arrays-in-C.c
+++ 1D-Arrays-in-C.c
@@ -3,20 +3,19 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main() {
-
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+int main() 
+  { 
     int sum=0;
     int i;
     int n;
     scanf("%d",&n);
     int arr[n];
     for(i=1;i<=n;i++)
-    {   scanf("%d",&arr[i]);
-        sum=sum+arr[i];
-        
-    }  
+     {  
+        scanf("%d",&arr[i]);
+        sum=sum+arr[i];  
+     }  
     printf("%d",sum);
-    return 0;
+  return 0;
 }
 
