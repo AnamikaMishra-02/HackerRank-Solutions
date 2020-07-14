ba746157b7066fc2cb7029255985617c8ff184c8
AnamikaMishra-02 <60975676+AnamikaMishra-02@users.noreply.github.com>
2020-07-14 19:38:23 +0530

    Update Functions-in-C.c

--- Functions-in-C.c
+++ Functions-in-C.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
-/*
-Add `int max_of_four(int a, int b, int c, int d)` here.
-*/
 
 int max_of_four(int a, int b, int c, int d)
-{int s;
+{
+   int s;
    if(a>=b && a>=c && a>=d)
      {
          s=a;
      }
-
    else if(b>=c && b>=d && b>=a)
       {
           s=b;
       }
-
    else if(c>=d && c>=a && c>=b )
    {
        s=c;
-    
    }
    else
    s=d;
    return(s);
 }
-int main() {
+int main() 
+ {
     int a, b, c, d;
     scanf("%d %d %d %d", &a, &b, &c, &d);
     int ans = max_of_four(a, b, c, d);
