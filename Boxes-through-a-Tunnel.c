fb26bd91db0fe6e8866d78a11580c796701b613d
AnamikaMishra-02 <60975676+AnamikaMishra-02@users.noreply.github.com>
2020-07-18 15:38:18 +0530

    Update Boxes-through-a-Tunnel.c

--- Boxes-through-a-Tunnel.c
+++ Boxes-through-a-Tunnel.c
@@ -4,34 +4,24 @@
 
 struct box
 {
-	/**
-	* Define three fields of type int: length, width and height
-	*/
+
     int length,width,height;
 };
 
 typedef struct box box;
 
-int get_volume(box b) {
-	/**
-	* Return the volume of the box
-	*/
+int get_volume(box b) 
+{
     return (b.length*b.width*b.height);
-    
-
 }
 
-int is_lower_than_max_height(box b) {
-	/**
-	* Return 1 if the box's height is lower than MAX_HEIGHT and 0 otherwise
-	*/
+int is_lower_than_max_height(box b)
+{
     int c,f;
     if(b.height<41 ) 
        return 1;
     else 
-        return 0;
-    
-   
+        return 0; 
 }
 
 int main()
@@ -39,13 +29,16 @@ int main()
 	int n;
 	scanf("%d", &n);
 	box *boxes = malloc(n * sizeof(box));
-	for (int i = 0; i < n; i++) {
-		scanf("%d%d%d", &boxes[i].length, &boxes[i].width, &boxes[i].height);
+	for (int i = 0; i < n; i++) 
+	{
+          scanf("%d%d%d", &boxes[i].length, &boxes[i].width, &boxes[i].height);
 	}
-	for (int i = 0; i < n; i++) {
-		if (is_lower_than_max_height(boxes[i])) {
-			printf("%d\n", get_volume(boxes[i]));
-		}
+	for (int i = 0; i < n; i++) 
+	{
+		if (is_lower_than_max_height(boxes[i])) 
+		   {
+	             printf("%d\n", get_volume(boxes[i]));
+		   }
 	}
 	return 0;
 }
