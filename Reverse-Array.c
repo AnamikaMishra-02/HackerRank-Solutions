96563b1e91312f312100014c46a30db4ad9f3531
AnamikaMishra-02 <60975676+AnamikaMishra-02@users.noreply.github.com>
2020-07-30 20:15:20 +0530

    30 Days Code Challenge

--- Reverse-Array.c
+++ Reverse-Array.c
@@ -1,19 +1,26 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-   int n,i;
-   scanf("%d",&n);
-   int arr[n];
-   for(i=0; i<=n; i++)
-   {
-       scanf("%d",&arr[i]);
-   }
-   for(i=n-1;i>=0;i--)
-   {
-       printf("%d",arr[i]);
-       printf(" ");
-   }
- return 0;
-} 
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+     for(int i=n-1;i>=0;i--)
+    {
+        printf("%d",arr[i]);
+        printf(" ");
+    }
+  return 0;
+}
