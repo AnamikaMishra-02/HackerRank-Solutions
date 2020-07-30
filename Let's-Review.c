8c0b40643036cfa7a09c2debf4b36367ad939605
AnamikaMishra-02 <60975676+AnamikaMishra-02@users.noreply.github.com>
2020-07-30 22:41:53 +0530

    30 Days Code Challenge

--- Let's-Review.c
+++ Let's-Review.c
@@ -3,36 +3,34 @@
 #include <math.h>
 #include <stdlib.h>
 
+int myfunction(char s[10001])
+  {
+        for(int i=0; i<strlen(s); i++)
+        {
+            if(i%2==0)
+              printf("%c",s[i]);
+        }
+        printf(" ");
+        for(int i=0; i<strlen(s); i++)
+        {
+            if(i%2 !=0)
+              printf("%c",s[i]);
+        }
+        printf("\n");
+     return 0;
+    
+   }
+
 int main() {
-
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
-    int n,c=0,d=0,i;
-    char str[10001],even[10001],odd[10001];
-    scanf("%d",&n);
-    int x=strlen(str);
-    scanf("%s",str);
-    while(n!=0)
-    {  
-        for(i=0 ; i<x; i++)
-         {
-           if(i%2==0)
-            {
-              even[c]==str[i];
-              c++;
-            }
-           else 
-           {
-             odd[d]==str[i];
-             d++;
-           }
-         }
-             printf("%s",even);
-             printf(" ");
-             printf("%s\n",odd);
-     n--;
-    }
-   
-       
-    return 0;
-}
+    int test_cases;
+    char s[10001];
+    scanf("%d",&test_cases);
+    for(int i=0; i<test_cases; i++)
+    {
+        scanf("%s",s);
+        myfunction(s);
 
+    }
+      
+}
