4874eeece2ea22414038339880d10b08053fabac
AnamikaMishra-02 <60975676+AnamikaMishra-02@users.noreply.github.com>
2020-09-20 12:31:20 +0530

    C++

---
+++ Simple-Array-sum.cpp
@@ -0,0 +1,14 @@
+int simpleArraySum(vector<int> ar) {
+    /*
+     * Write your code here.
+     */
+     int n=ar.size();
+    int sum=0;
+     // Write your code here.
+     for(int i=0;i<n;i++)
+     {
+         sum=sum+ar[i];
+     }
+     return sum;
+
+}
