cc35a5cb235862199b739b6cac5e8c496de4b049
AnamikaMishra-02 <60975676+AnamikaMishra-02@users.noreply.github.com>
2020-09-27 23:25:30 +0530

    Problem Solving

---
+++ StairCase.cpp
@@ -0,0 +1,17 @@
+void staircase(int n) 
+{
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+      for(int j=i;j<n;j++)
+      {  
+          cout<<" ";
+      }
+      for(int j=1;j<=i;j++)
+      {
+          cout<<"#";
+      }
+      cout<<endl;
+    }
+
+}
