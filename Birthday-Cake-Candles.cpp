d3c54059f7b1a43f20e113f5e98279c2062597a6
AnamikaMishra-02 <60975676+AnamikaMishra-02@users.noreply.github.com>
2020-09-28 23:39:09 +0530

    Problem Solving

--- Birthday-Cake-Candles.cpp
+++ Birthday-Cake-Candles.cpp
@@ -10,8 +10,8 @@ int birthdayCakeCandles(vector<int> candles)
    int count=0;
    for(int i=0;i<n;i++)
    {
-       if(candles[i]==max)
-         count++;
+        if(candles[i]==max)
+          count++;
    }
   return count;
 }
